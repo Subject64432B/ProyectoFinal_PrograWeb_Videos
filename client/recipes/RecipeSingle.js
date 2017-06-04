9183283c486a6157f94bb7078285f8bf8769316a
SamuelVZ <zamy-sama@hotmail.com>
2017-06-04 00:23:48 -0600

    FlowRouter parametros

---
+++ client/recipes/RecipeSingle.js
@@ -0,0 +1,16 @@
+//Meteor.subscribe('recipes');
+
+Template.RecipeSingle.onCreated(function() {
+	var self = this;
+	self.autorun(function(){
+		self.subscribe('recipes');
+	});
+
+});
+
+Template.RecipeSingle.helpers({
+	recipe: ()=> {
+		var id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+});
